5456f20e40b7cef62e67fabad41d7ee89f6f10da
Gullit Miranda <gullitmiranda@gmail.com>
2015-03-02 03:23:13 -0300

    [auth] Enable session to all domain

--- auth/config/initializers/session_store.rb
+++ auth/config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_auth_session'
+Rails.application.config.session_store :cookie_store, key: '_meli_session',
+                                                      domain: :all
